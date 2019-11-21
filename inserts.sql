ec838a529afeb97b899275817327140bacddacf0
Jacob <jcthomas556@byui.com>
2019-11-21 15:42:57 -0700

    live

---
+++ inserts.sql
@@ -0,0 +1,9 @@
+INSERT into player_characters (player_fname, player_lname, player_ac, player_init_bonus, player_race, player_class) 
+VALUES (
+    'Saul',
+    'Solarian',
+    '14',
+    '1',
+    'Aasimar',
+    'Sorcerer'
+    );
--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const express = require('express');//install this with npm install express in the terminal, also instal ejs
 var app = express();
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 const {Pool} = require('pg');
-const pool = new Pool({connectionString: process.env.DATABASE_URL || 'postgres://rsmhlcvwmrbznt:fca674e938383ee27c1e601e38264bfe378d950111d4fa6e0622ed54ae604d72@ec2-174-129-203-86.compute-1.amazonaws.com:5432/d5no66ko5da4s0?ssl=true'})
+const pool = new Pool({connectionString: process.env.DATABASE_URL || 'postgres://ntwzgxxvnkywoq:ce7468ac2b9ce2c01e407f70d620eac576ac4aa059507db9262fae872ed5932f@ec2-174-129-229-162.compute-1.amazonaws.com:5432/dfb7aff5tskn4d?ssl=true'})
 
 app.set('port', process.env.PORT || 5000)
     .use(express.static(__dirname + '/public'))//this lets me put files in the public folder that are acceable globally
     .set('views', __dirname + '/views')
     .set('view engine', 'ejs')
-    .get('/getPerson', function(req, res){
-        pool.query('SELECT * FROM person WHERE id = $1', [req.query.id], function(err, result) {
-            if (err) {
-                console.log("Error in query: ")
-                console.log(err);
-            }
-            console.log("Back from DB with result:");
-            console.log(result.rows);
-        })
+    .get('/', function(req, res){
+        console.log('success');
+        res.sendFile('home.html', { root: __dirname + "/public"})
+        // pool.query('SELECT * FROM player_characters', function(err, result) {
+        //     if (err) {
+        //         console.log("Error in query: ")
+        //         console.log(err);
+        //     }
+        //     console.log("Back from DB with result:");
+        //     console.log(result.rows);
+        //     res.send(result.rows);
+        // })
     })
     .listen(app.get('port'), function(){
         console.log('Listening on port: ' + app.get('port'));
